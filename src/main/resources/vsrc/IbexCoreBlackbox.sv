df0f790c9ff487078ac56232e236d57f78c52acd
Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>
2021-10-26 21:48:09 -0700

    Add license

--- src/main/resources/vsrc/IbexCoreBlackbox.sv
+++ src/main/resources/vsrc/IbexCoreBlackbox.sv
@@ -1,36 +1,46 @@
+//******************************************************************************
+// Copyright (c) 2021 - 2021, The Regents of the University of California (Regents).
+// All Rights Reserved. See LICENSE for license details.
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Ibex Core Wrapper
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
 module IbexCoreBlackbox
-	#(
-		parameter MHPM_COUNTER_NUM = 0,
-		parameter MHPM_COUNTER_WIDTH = 0,
-		parameter RV32E = 0,
-		parameter RV32M = ibex_pkg::RV32MFast,
-    	parameter RV32B = ibex_pkg::RV32BNone,
-    	parameter REGFILE = ibex_pkg::RegFileFF,
-    	parameter BRANCH_TARGET_ALU = 0,
-    	parameter WB_STAGE = 0,
-    	parameter BRANCH_PREDICTOR = 0,
-    	parameter PMP_ENABLE = 0,
-    	parameter PMP_GRANULARITY = 0,
-		parameter PMP_NUM_REGIONS = 0,
-		parameter DBG_HW_BREAK_NUM = 0,
-		parameter [31:0] DM_HALT_ADDR = 0,
-		parameter [31:0] DM_EXCEPTION_ADDR = 0
-	) 
+    #(
+        parameter MHPM_COUNTER_NUM = 0,
+        parameter MHPM_COUNTER_WIDTH = 0,
+        parameter RV32E = 0,
+        parameter RV32M = ibex_pkg::RV32MFast,
+        parameter RV32B = ibex_pkg::RV32BNone,
+        parameter REGFILE = ibex_pkg::RegFileFF,
+        parameter BRANCH_TARGET_ALU = 0,
+        parameter WB_STAGE = 0,
+        parameter BRANCH_PREDICTOR = 0,
+        parameter PMP_ENABLE = 0,
+        parameter PMP_GRANULARITY = 0,
+        parameter PMP_NUM_REGIONS = 0,
+        parameter DBG_HW_BREAK_NUM = 0,
+        parameter [31:0] DM_HALT_ADDR = 0,
+        parameter [31:0] DM_EXCEPTION_ADDR = 0
+    ) 
 ( 
-	//inputs/outputs
-	// Clock and Reset
-    input clk_i,
-    input rst_ni,
+    // Clock and Reset
+    input  clk_i,
+    input  rst_ni,
 
-    input test_en_i,     // enable all clock gates for testing
+    input  test_en_i,     		// enable all clock gates for testing
 
-    input ram_cfg_i_ram_cfg_en,
-    input [3:0] ram_cfg_i_ram_cfg,
-    input ram_cfg_i_rf_cfg_en,
-    input [3:0] ram_cfg_i_rf_cfg,
+    input  ram_cfg_i_ram_cfg_en,
+    input  [3:0] ram_cfg_i_ram_cfg,
+    input  ram_cfg_i_rf_cfg_en,
+    input  [3:0] ram_cfg_i_rf_cfg,
 
-    input [31:0] hart_id_i,
-    input [31:0] boot_addr_i,
+    input  [31:0] hart_id_i,
+    input  [31:0] boot_addr_i,
 
     // Instruction memory interface
     output instr_req_o,
@@ -52,14 +62,14 @@ module IbexCoreBlackbox
     input  data_err_i,
 
     // Interrupt inputs
-    input irq_software_i, 		//msip
-    input irq_timer_i, 			//mtip
-    input irq_external_i, 		//meip
-    input [14:0] irq_fast_i, 	//fast local interrupts
-    input irq_nm_i,       		//nmi
+    input  irq_software_i,      //msip
+    input  irq_timer_i,         //mtip
+    input  irq_external_i,      //meip
+    input  [14:0] irq_fast_i,   //fast local interrupts
+    input  irq_nm_i,            //nmi
 
     // Debug Interface
-    input debug_req_i, //debug
+    input  debug_req_i, //debug
 
     //debugging output for ibex_lockstep.sv
     output [31:0] crash_dump_o_current_pc,
@@ -68,72 +78,72 @@ module IbexCoreBlackbox
     output [31:0] crash_dump_o_exception_addr,
 
     // CPU Control Signals
-    input fetch_enable_i,
+    input  fetch_enable_i,
     output alert_minor_o,
     output alert_major_o,
     output core_sleep_o,
 
     // DFT bypass controls
-    input scan_rst_ni
+    input  scan_rst_ni
 );
 
-	prim_ram_1p_pkg::ram_1p_cfg_t ibex_ram_config;
-	ibex_pkg::crash_dump_t ibex_crash_dump;
+    prim_ram_1p_pkg::ram_1p_cfg_t ibex_ram_config;
+    ibex_pkg::crash_dump_t ibex_crash_dump;
 
-	ibex_top #(
-		.PMPEnable(PMP_ENABLE),
-	    .PMPGranularity(PMP_GRANULARITY),
-	    .PMPNumRegions(PMP_NUM_REGIONS),
-	    .MHPMCounterNum(MHPM_COUNTER_NUM),
-	    .MHPMCounterWidth(MHPM_COUNTER_WIDTH),
-	    .RV32E(RV32E),
-	    .RV32M(RV32M),
-	    .RV32B(RV32B),
-	    .RegFile(REGFILE),
-	    .BranchTargetALU(BRANCH_TARGET_ALU),
-	    .WritebackStage(WB_STAGE),
-	    .ICache(1'b0),
-	    .ICacheECC(1'b0),
-	    .BranchPredictor(BRANCH_PREDICTOR),
-	    .DbgTriggerEn(1'b0),
-	    .DbgHwBreakNum(DBG_HW_BREAK_NUM),
-	    .SecureIbex(1'b0),
-	    .DmHaltAddr(DM_HALT_ADDR),
-	    .DmExceptionAddr(DM_EXCEPTION_ADDR)
-	) i_ibex (
-	    .clk_i,
-	    .rst_ni,
-	    .test_en_i,
-	    .ram_cfg_i ( ibex_ram_config ),
-	    .hart_id_i,
-	    .boot_addr_i,
-		.instr_req_o,
-	    .instr_gnt_i,
-	    .instr_rvalid_i,
-	    .instr_addr_o,
-	    .instr_rdata_i,
-	    .instr_err_i,
-		.data_req_o,
-	    .data_gnt_i,
-	    .data_rvalid_i,
-	    .data_we_o,
-	    .data_be_o,
-	    .data_addr_o,
-	    .data_wdata_o,
-	    .data_rdata_i,
-	    .data_err_i,
-		.irq_software_i,
-	    .irq_timer_i,
-	    .irq_external_i,
-	    .irq_fast_i,
-	    .irq_nm_i,
-	    .debug_req_i,
-	    .crash_dump_o ( ibex_crash_dump ),
-	    .fetch_enable_i,
-	    .alert_minor_o,
-	    .alert_major_o,
-	    .core_sleep_o,
-	    .scan_rst_ni
-	);
+    ibex_top #(
+        .PMPEnable(PMP_ENABLE),
+        .PMPGranularity(PMP_GRANULARITY),
+        .PMPNumRegions(PMP_NUM_REGIONS),
+        .MHPMCounterNum(MHPM_COUNTER_NUM),
+        .MHPMCounterWidth(MHPM_COUNTER_WIDTH),
+        .RV32E(RV32E),
+        .RV32M(RV32M),
+        .RV32B(RV32B),
+        .RegFile(REGFILE),
+        .BranchTargetALU(BRANCH_TARGET_ALU),
+        .WritebackStage(WB_STAGE),
+        .ICache(1'b0),
+        .ICacheECC(1'b0),
+        .BranchPredictor(BRANCH_PREDICTOR),
+        .DbgTriggerEn(1'b0),
+        .DbgHwBreakNum(DBG_HW_BREAK_NUM),
+        .SecureIbex(1'b0),
+        .DmHaltAddr(DM_HALT_ADDR),
+        .DmExceptionAddr(DM_EXCEPTION_ADDR)
+    ) i_ibex (
+        .clk_i,
+        .rst_ni,
+        .test_en_i,
+        .ram_cfg_i ( ibex_ram_config ),
+        .hart_id_i,
+        .boot_addr_i,
+        .instr_req_o,
+        .instr_gnt_i,
+        .instr_rvalid_i,
+        .instr_addr_o,
+        .instr_rdata_i,
+        .instr_err_i,
+        .data_req_o,
+        .data_gnt_i,
+        .data_rvalid_i,
+        .data_we_o,
+        .data_be_o,
+        .data_addr_o,
+        .data_wdata_o,
+        .data_rdata_i,
+        .data_err_i,
+        .irq_software_i,
+        .irq_timer_i,
+        .irq_external_i,
+        .irq_fast_i,
+        .irq_nm_i,
+        .debug_req_i,
+        .crash_dump_o ( ibex_crash_dump ),
+        .fetch_enable_i,
+        .alert_minor_o,
+        .alert_major_o,
+        .core_sleep_o,
+        .scan_rst_ni
+    );
 
 endmodule
--- src/main/scala/IbexCoreBlackbox.scala
+++ src/main/scala/IbexCoreBlackbox.scala
@@ -1,3 +1,14 @@
+//******************************************************************************
+// Copyright (c) 2021 - 2021, The Regents of the University of California (Regents).
+// All Rights Reserved. See LICENSE for license details.
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+// Ibex Tile Wrapper
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
 package ibex
 
 import sys.process._
@@ -8,99 +19,86 @@ import chisel3.experimental.{IntParam, StringParam, RawParam}
 
 import scala.collection.mutable.{ListBuffer}
 
-import freechips.rocketchip.config._
-import freechips.rocketchip.subsystem._
-import freechips.rocketchip.devices.tilelink._
-import freechips.rocketchip.diplomacy._
-import freechips.rocketchip.diplomaticobjectmodel.logicaltree.{LogicalModuleTree, LogicalTreeNode, RocketLogicalTreeNode, ICacheLogicalTreeNode}
-import freechips.rocketchip.rocket._
-import freechips.rocketchip.subsystem.{RocketCrossingParams}
-import freechips.rocketchip.tilelink._
-import freechips.rocketchip.interrupts._
-import freechips.rocketchip.util._
-import freechips.rocketchip.tile._
-import freechips.rocketchip.amba.axi4._
-
 class IbexCoreBlackbox(
-	pmpEnable: Int,
-	pmpGranularity: Int,
-	pmpNumRegions: Int,
-	mhpmCounterNum: Int,
-	mhpmCounterWidth: Int,
-	rv32e: Int,
-	rv32m: String,
-	rv32b: String,
-	regfile: String,
-	branchTargetALU: Int,
-	wbStage: Int,
-	branchPredictor: Int,
-	dbgHwBreakNum: Int,
-	dmHaltAddr: Int,
-	dmExceptionAddr: Int)
-	extends BlackBox(
-		Map(
-			"PMP_ENABLE" -> IntParam(pmpEnable),
-			"PMP_GRANULARITY" -> IntParam(pmpGranularity),
-			"PMP_NUM_REGIONS" -> IntParam(pmpNumRegions),
-			"MHPM_COUNTER_NUM" -> IntParam(mhpmCounterNum),
-			"MHPM_COUNTER_WIDTH" -> IntParam(mhpmCounterWidth),
-			"RV32E" -> IntParam(rv32e),
-			"RV32M" -> RawParam(rv32m),
-    		"RV32B" -> RawParam(rv32b),
-    		"REGFILE" -> RawParam(regfile),
-    		"BRANCH_TARGET_ALU" -> IntParam(branchTargetALU),
-    		"WB_STAGE" -> IntParam(wbStage),
-    		"BRANCH_PREDICTOR" -> IntParam(branchPredictor),
-			"DBG_HW_BREAK_NUM" -> IntParam(dbgHwBreakNum),
-			"DM_HALT_ADDR" -> IntParam(dmHaltAddr),
-			"DM_EXCEPTION_ADDR" -> IntParam(dmExceptionAddr))
+    pmpEnable: Int,
+    pmpGranularity: Int,
+    pmpNumRegions: Int,
+    mhpmCounterNum: Int,
+    mhpmCounterWidth: Int,
+    rv32e: Int,
+    rv32m: String,
+    rv32b: String,
+    regfile: String,
+    branchTargetALU: Int,
+    wbStage: Int,
+    branchPredictor: Int,
+    dbgHwBreakNum: Int,
+    dmHaltAddr: Int,
+    dmExceptionAddr: Int)
+    extends BlackBox(
+        Map(
+            "PMP_ENABLE" -> IntParam(pmpEnable),
+            "PMP_GRANULARITY" -> IntParam(pmpGranularity),
+            "PMP_NUM_REGIONS" -> IntParam(pmpNumRegions),
+            "MHPM_COUNTER_NUM" -> IntParam(mhpmCounterNum),
+            "MHPM_COUNTER_WIDTH" -> IntParam(mhpmCounterWidth),
+            "RV32E" -> IntParam(rv32e),
+            "RV32M" -> RawParam(rv32m),
+            "RV32B" -> RawParam(rv32b),
+            "REGFILE" -> RawParam(regfile),
+            "BRANCH_TARGET_ALU" -> IntParam(branchTargetALU),
+            "WB_STAGE" -> IntParam(wbStage),
+            "BRANCH_PREDICTOR" -> IntParam(branchPredictor),
+            "DBG_HW_BREAK_NUM" -> IntParam(dbgHwBreakNum),
+            "DM_HALT_ADDR" -> IntParam(dmHaltAddr),
+            "DM_EXCEPTION_ADDR" -> IntParam(dmExceptionAddr))
 )
-	with HasBlackBoxResource
+    with HasBlackBoxResource
 {
-	val io = IO(new Bundle {
-		val clk_i = Input(Clock())
-    	val rst_ni = Input(Bool())
-    	val test_en_i = Input(Bool())
-    	val ram_cfg_i_ram_cfg_en = Input(Bool())
-    	val ram_cfg_i_ram_cfg = Input(UInt(4.W))
-    	val ram_cfg_i_rf_cfg_en = Input(Bool())
-    	val ram_cfg_i_rf_cfg = Input(UInt(4.W))
-    	val hart_id_i = Input(UInt(32.W))
-    	val boot_addr_i = Input(UInt(32.W))
-    	val instr_req_o = Output(Bool())
-    	val instr_gnt_i = Input(Bool())
-    	val instr_rvalid_i = Input(Bool())
-    	val instr_addr_o = Output(UInt(32.W))
-    	val instr_rdata_i = Input(UInt(32.W))
-    	val instr_err_i = Input(Bool())
-    	val data_req_o = Output(Bool())
-    	val data_gnt_i = Input(Bool())
-    	val data_rvalid_i = Input(Bool())
-    	val data_we_o = Output(Bool())
-    	val data_be_o = Output(UInt(4.W))
-    	val data_addr_o = Output(UInt(32.W))
-    	val data_wdata_o = Output(UInt(32.W))
-    	val data_rdata_i = Input(UInt(32.W))
-    	val data_err_i = Input(Bool())
-    	val irq_software_i = Input(Bool())
-    	val irq_timer_i = Input(Bool())
-    	val irq_external_i = Input(Bool())
-    	val irq_fast_i = Input(UInt(15.W))
-    	val irq_nm_i = Input(Bool())
-    	val debug_req_i = Input(Bool())
-    	val crash_dump_o_current_pc = Output(UInt(32.W))
-    	val crash_dump_o_next_pc = Output(UInt(32.W))
-    	val crash_dump_o_last_data_addr = Output(UInt(32.W))
-    	val crash_dump_o_exception_addr = Output(UInt(32.W))
-    	val fetch_enable_i = Input(Bool())
-    	val alert_minor_o = Output(Bool())
-    	val alert_major_o = Output(Bool())
-    	val core_sleep_o = Output(Bool())
-    	val scan_rst_ni = Input(Bool())
-	})
+    val io = IO(new Bundle {
+        val clk_i = Input(Clock())
+        val rst_ni = Input(Bool())
+        val test_en_i = Input(Bool())
+        val ram_cfg_i_ram_cfg_en = Input(Bool())
+        val ram_cfg_i_ram_cfg = Input(UInt(4.W))
+        val ram_cfg_i_rf_cfg_en = Input(Bool())
+        val ram_cfg_i_rf_cfg = Input(UInt(4.W))
+        val hart_id_i = Input(UInt(32.W))
+        val boot_addr_i = Input(UInt(32.W))
+        val instr_req_o = Output(Bool())
+        val instr_gnt_i = Input(Bool())
+        val instr_rvalid_i = Input(Bool())
+        val instr_addr_o = Output(UInt(32.W))
+        val instr_rdata_i = Input(UInt(32.W))
+        val instr_err_i = Input(Bool())
+        val data_req_o = Output(Bool())
+        val data_gnt_i = Input(Bool())
+        val data_rvalid_i = Input(Bool())
+        val data_we_o = Output(Bool())
+        val data_be_o = Output(UInt(4.W))
+        val data_addr_o = Output(UInt(32.W))
+        val data_wdata_o = Output(UInt(32.W))
+        val data_rdata_i = Input(UInt(32.W))
+        val data_err_i = Input(Bool())
+        val irq_software_i = Input(Bool())
+        val irq_timer_i = Input(Bool())
+        val irq_external_i = Input(Bool())
+        val irq_fast_i = Input(UInt(15.W))
+        val irq_nm_i = Input(Bool())
+        val debug_req_i = Input(Bool())
+        val crash_dump_o_current_pc = Output(UInt(32.W))
+        val crash_dump_o_next_pc = Output(UInt(32.W))
+        val crash_dump_o_last_data_addr = Output(UInt(32.W))
+        val crash_dump_o_exception_addr = Output(UInt(32.W))
+        val fetch_enable_i = Input(Bool())
+        val alert_minor_o = Output(Bool())
+        val alert_major_o = Output(Bool())
+        val core_sleep_o = Output(Bool())
+        val scan_rst_ni = Input(Bool())
+    })
 
-	val proc = "make -C generators/ibex/src/main/resources/vsrc default"
-	require (proc.! == 0, "Failed to run preprocessing step")
+    val proc = "make -C generators/ibex/src/main/resources/vsrc default"
+    require (proc.! == 0, "Failed to run preprocessing step")
 
-	addResource("/vsrc/IbexCoreBlackbox.preprocessed.sv")
+    addResource("/vsrc/IbexCoreBlackbox.preprocessed.sv")
 }
